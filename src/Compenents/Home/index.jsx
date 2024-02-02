a27e30c27d9e1c501ea2ba9f0c5cc34d579f6fb9
danishyaqoob <danishzeevhy@gmail.com>
2024-02-02 19:28:34 +0500

    qoute issue

--- src/Compenents/Home/index.jsx
+++ src/Compenents/Home/index.jsx
@@ -9,9 +9,9 @@ const HomeSection = () => {
             <div id="home" className=" w-full">
                 <div className="flex gap-[36px] items-center max-w-[1240px] lg:flex-row flex-col px-[14px] sm:px-[20px] mx-auto pt-[70px] sm:pt-[80px] md:pt-[100px] ">
                     <div className="flex-1 ">
-                    <h3 className="text-[20px] md:text-[22px] text-[#fff] font-[700] ">Hello, it's Me</h3>
+                    <h3 className="text-[20px] md:text-[22px] text-[#fff] font-[700] ">Hello, it&apos;s Me</h3>
                     <h2 className="text-[40px] md:text-[50px] text-[#fff] font-[900] ">Danish Yaqoob</h2>
-                    <h2 className="text-[20px] md:text-[25px] text-[#fff] font-[700] flex items-center gap-[4px] line-clamp-1 "><p className=" whitespace-nowrap">and i'm a</p> <p className="text-[#01eefe] line-clamp-1 "> Frontend Web Developer</p></h2>
+                    <h2 className="text-[20px] md:text-[25px] text-[#fff] font-[700] flex items-center gap-[4px] line-clamp-1 "><p className=" whitespace-nowrap">and i&apos;m a</p> <p className="text-[#01eefe] line-clamp-1 "> Frontend Web Developer</p></h2>
                     <p className="text-[#ffffffbe] text-[12px] md:text-[13px] sm:text-[15px] mt-[16px] md:mt-[20px] font-[500] ">Crafting intuitive user interfaces with precision and creativity, bringing concepts to life through code and design. Dedicated to optimizing user experiences with responsive, interactive web solutions.</p>
                     <div className="flex items-center gap-[14px] mt-[22px] ">
                         <Link target="blank" href={''} className="rounded-[50%] border-[2px] border-[#01eefe] shadow transorm-scale transition-three-second cursor-pointer flex items-center justify-center h-[28px] md:h-[30px] w-[28px] md:w-[30px] "><i class="fa-brands fa-linkedin-in text-[#01eefe] text-[12px] md:text-[14px] "></i></Link>
